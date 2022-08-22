041c26f529d046fd9c676aa5ad66766a012acaae
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-08-22 09:25:06 -0300

    feat: WorkTools exercise upload

---
+++ WorkTools/WorkTools/WorkTools.cs
@@ -0,0 +1,16 @@
+﻿namespace WorkTools;
+
+public static class NumbersTools
+{
+  public static int CountOf(int[] numbers, int value)
+  {
+    var count = 0;
+
+    foreach (var number in numbers)
+    {
+      if (number == value) count++;
+    }
+
+    return count;
+  }
+}
