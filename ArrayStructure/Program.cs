5c146d240013d58789cb90d539441d020f9bee45
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-08-28 19:16:34 -0300

    feat: the array structure exercises

---
+++ ArrayStructure/Program.cs
@@ -0,0 +1,11 @@
+﻿//Criando o array
+int[] myFirstArray;
+
+//instanciando array: 
+//array = new tipo[tamanhoDoArray];
+myFirstArray = new int[5];
+Console.WriteLine(myFirstArray);
+
+// inicializando e instanciando o array de forma simplificada
+int[] myFirstArray2 = new int[5];
+Console.WriteLine(myFirstArray2);
