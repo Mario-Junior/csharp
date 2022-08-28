c475577dd52dcf51cb3f7965a97b583c00a2dc04
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-08-28 20:09:49 -0300

    feat: the array structure exercises

--- ArrayStructure/Program.cs
+++ ArrayStructure/Program.cs
@@ -38,7 +38,44 @@ foreach (var word in myFirstStringArray2)
 // Um Array em C# é imutável, ou seja, assim que a instância de um Array é criada, o seu tamanho não pode ser alterado.
 // Porém ele pode ser instanciado novamente usando um valor diferente com uma nova atribuição =.
 int[] myArrayInit = {1, 2, 3, 4};
+Console.WriteLine("myArrayInit length = " + myArrayInit.Length);
 foreach (var number in myArrayInit)
 {
   Console.WriteLine(number);
+}
+
+myArrayInit = new int[10];
+Console.WriteLine("myArrayInit length = " + myArrayInit.Length);
+
+// Arrays multidimensionais
+PlayingWithArrays.commonMultiDimensionalArray();
+PlayingWithArrays.jaggedMultiDimensionalArray();
+
+class PlayingWithArrays
+{
+  public static void commonMultiDimensionalArray()
+  {
+    int[,] commonMultiDimensionalArray = new int[3, 3] {{1,2,3}, {1,2,3}, {1,2,3}};
+    for (int i = 0; i < commonMultiDimensionalArray.GetLength(0); i++)
+    {
+      for (int j = 0; j < commonMultiDimensionalArray.GetLength(1); j++)
+      {
+        Console.Write("{0} ", commonMultiDimensionalArray[i, j]);
+      }
+      Console.WriteLine();
+    }
+  }
+
+  public static void jaggedMultiDimensionalArray()
+  {
+    int[,] jaggedMultiDimensionalArray = new int[3, 3] {{1,2,3}, {1,2,3}, {1,2,3}};
+    for (int i = 0; i < jaggedMultiDimensionalArray.GetLength(0); i++)
+    {
+      for (int j = 0; j < jaggedMultiDimensionalArray.GetLength(1); j++)
+      {
+        Console.Write("{0} ", jaggedMultiDimensionalArray[i, j]);
+      }
+      Console.WriteLine();
+    }
+  }
 }
