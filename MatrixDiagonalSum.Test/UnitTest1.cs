5f941a1eb4ecd04ce13ba7f594bd2a55fbe57cbf
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-08-29 09:31:48 -0300

    feat: the array structure exercises

--- MatrixDiagonalSum.Test/UnitTest1.cs
+++ MatrixDiagonalSum.Test/UnitTest1.cs
@@ -2,13 +2,24 @@ namespace MatrixDiagonalSum.Test;
 
 public class TestMatrixDiagonalSum
 {
-    [Theory(DisplayName = "Imprime todos os elementos de um Array")]
+    [Theory(DisplayName = "1- Imprime todos os elementos de um Array de elementos iguais")]
     [InlineData(new int[] { 1, 2, 3, 4 })]
-    public void TestArrayInlineData(int[] intArrayEntry)
+    public void TestArrayInlineData1(int[] intArrayEntry)
     {
         for (int i = 0; i < intArrayEntry.Length; i++)
         {
             Console.WriteLine(intArrayEntry[i]);
         }
+    }
+
+    [Theory(DisplayName = "1- Imprime todos os elementos de um Array com elementos diferentes")]
+    [InlineData(new int[] { 1, 2, 3, 4 }, "Minha string")]
+    public void TestArrayInlineData2(int[] intArrayEntry, string stringEntry)
+    {
+        for (int i = 0; i < intArrayEntry.Length; i++)
+        {
+            Console.WriteLine(intArrayEntry[i]);
+        }
+        Console.WriteLine(stringEntry);
     }
 }
