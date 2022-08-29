ca88692ab8d6199c0d05cd74b2fcc83eb8c50de3
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-08-29 09:15:16 -0300

    feat: the array structure exercises

--- MatrixDiagonalSum/Program.cs
+++ MatrixDiagonalSum/Program.cs
@@ -2,18 +2,29 @@
 {
   public static void Main()
   {
-    int[,] myArray = new int[2, 3]
+    int[,] myArray = new int[3, 3]
     {
-      {1, 2 ,3},
+      {1, 2, 3},
       {4, 5, 6},
+      {7, 8, 9},
     };
 
-    Console.WriteLine(myArray.GetLength(0));
+    Console.WriteLine(myArray.GetLength(1));
+
+    int result = GetDiagonalSum(myArray);
+
+    Console.WriteLine("Diagonal Sum = " + result);
   }
 
-  // public static int GetDiagonalSum(int[,] matrix)
-  // {
-  //   int sum = 0;
+  public static int GetDiagonalSum(int[,] matrix)
+  {
+    int sum = 0;
 
-  // }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      sum += matrix[i, i];
+    }
+
+    return sum;
+  }
 }
