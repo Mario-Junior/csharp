120a1bc11ca2dfa35a47f456f5e54c6341e809c2
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-08-30 11:28:02 -0300

    feat: c# functions exercises

---
+++ Functions/PerformanceHall.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace PerformanceHall.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Functions/PerformanceHall/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AgeNamespace;
+
+class PerformanceHall
+{
+  public static void Main()
+  {
+    var age = CalculateAgeByYear(1982);
+    var isOfLegalAge = ValidateComingOfAge(age);
+  }
+  public static int CalculateAgeByYear(int yearOfBitrh)
+  {
+    return DateTime.Now.Year - yearOfBitrh;
+  }
+
+  public static bool ValidateComingOfAge(int age)
+  {
+    return age >= 18;
+  }
+}
