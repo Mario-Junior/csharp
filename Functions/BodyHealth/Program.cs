f888586a44fdc262325f9a051425738cd205946b
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-08-30 10:48:17 -0300

    feat: c# functions exercises

---
+++ Functions/BodyHealth/Program.cs
@@ -0,0 +1,12 @@
+﻿var instance = new BodyHealth();
+var bmi = instance.CalculateBMI(82, 1.78);
+
+Console.WriteLine(bmi);
+
+class BodyHealth
+{
+  public double CalculateBMI(int weight, double height)
+  {
+    return weight / (height * height);
+  }
+}
