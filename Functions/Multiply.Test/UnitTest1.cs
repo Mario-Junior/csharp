8445f60073485063a611560bd254de712ead8312
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-08-30 12:05:31 -0300

    feat: c# functions exercises

--- Functions/Multiply.Test/UnitTest1.cs
+++ Functions/Multiply.Test/UnitTest1.cs
@@ -1,10 +1,22 @@
 namespace Multiply.Test;
 
-public class UnitTest1
+public class MultiplyTest
 {
-    [Fact]
-    public void Test1()
-    {
 
+    public static int Multiply(int numberOne, int numberTwo)
+    {
+        return numberOne * numberTwo;
+    }
+
+    [Theory(DisplayName = "Teste para a multiplicação de dois números inteiros")]
+    [InlineData(2, 6, 12)]
+    [InlineData(6, 4, 24)]
+    [InlineData(4, 132, 528)]
+    [InlineData(999, 0, 0)]
+    public void TestMultiplyCalculator(int numberOne, int numberTwo, int resultExpected)
+    {
+        var calculationResult = Multiply(numberOne, numberTwo);
+
+        calculationResult.Should().Be(resultExpected);
     }
 }
