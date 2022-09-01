b62aa9db375d1f6486b59806b8d5f831db41b7bf
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-01 11:12:08 -0300

    feat: c# exceptions exercises

---
+++ 09-csharp-exceptions/MathSystem/Program.cs
@@ -0,0 +1,7 @@
+﻿int DivideNumbers(int numberOne, int numberTwo)
+{
+  return numberOne / numberTwo;
+}
+
+Console.WriteLine("Resultado 1 = " + DivideNumbers(12, 2));
+Console.WriteLine("Resultado 2 = " + DivideNumbers(12, 0)); // Erro: Unhandled exception. System.DivideByZeroException: Attempted to divide by zero.
