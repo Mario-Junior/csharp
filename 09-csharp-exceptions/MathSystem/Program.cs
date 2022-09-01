83e404f3ee37eeb2992436df7260a7591b44008c
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-01 11:22:43 -0300

    feat: c# exceptions exercises

--- 09-csharp-exceptions/MathSystem/Program.cs
+++ 09-csharp-exceptions/MathSystem/Program.cs
@@ -9,10 +9,16 @@ try
 {
   DivideNumbers(12, 0);
 }
+// catch Específico (para caso a exceção seja um Objeto do tipo do parâmetro passado no catch):
+catch (DivideByZeroException except)
+{
+  Console.WriteLine($"Ocorreu um erro específico: {except.Message}");
+}
 // catch Genérico com Objeto "Exception" base:
+// ATENÇÃO: Numa sequência de catches, deve ir por último, pois por ser genérica, captura todos os tipos!!!
 catch (Exception except)
 {
-  Console.WriteLine($"Ocorreu um erro: {except.Message}");
+  Console.WriteLine($"Ocorreu um erro genérico: {except.Message}");
 }
 
 Console.WriteLine("Resultado 2 = " + DivideNumbers(12, 0)); // Erro: Unhandled exception. System.DivideByZeroException: Attempted to divide by zero.
