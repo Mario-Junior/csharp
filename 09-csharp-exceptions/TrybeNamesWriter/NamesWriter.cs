1e72ebcf74b42829f756718f027fb9ccddbb5e35
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-03 11:14:34 -0300

    feat: c# exceptions exercises

---
+++ 09-csharp-exceptions/TrybeNamesWriter/NamesWriter.cs
@@ -0,0 +1,29 @@
+﻿var instance = new Nameswriter();
+
+instance.WriteName("Tryber");
+
+instance.WriteName(null);
+
+public class Nameswriter
+{
+
+  // public static void Main() {}
+
+  public void WriteName(string name)
+  {
+    try
+    {
+      if (name == null)
+      {
+        throw new ArgumentNullException(name, "Nome não pode ser um valor nulo.");
+      }
+
+      Console.WriteLine(string.Concat("Seu nome é: ", name));
+    }
+    catch (ArgumentNullException exc)
+    {
+      Console.WriteLine(exc.Message);
+      throw exc;
+    }
+  }
+}
