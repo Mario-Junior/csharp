0ccd53dbe82385646b79bbc5f0805d7340e7586e
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-07 11:20:25 -0300

    feat: abstraction - the classes in c# - exercises

--- 11-abstraction-the-classes-in-csharp/TryRockets/TryRockets.cs
+++ 11-abstraction-the-classes-in-csharp/TryRockets/TryRockets.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace TryRockets;
+
+class Rocket
+{
+  string Name { get; set; }
+  int Fuel { get; set; }
+  decimal Price { get; set; }
+
+  public Rocket(string name, decimal price)
+  {
+    Name = name;
+    Fuel = 0;
+    Price = price;
+  }
+}
