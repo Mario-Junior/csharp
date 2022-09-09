7ab81dcea2aa2319c2e9b5f50f17887edce0838d
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-09 11:09:37 -0300

    feat: static classes and methods exercises

---
+++ 12-static-classes-and-methods/Rocket/Program.cs
@@ -0,0 +1,16 @@
+﻿using RocketFactory;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    Rocket rocket1 = new();
+    rocket1.FuelAsPercentage = 10;
+    Console.WriteLine(rocket1.FuelAsPercentage); // Saída esperada: 10 %
+    Console.WriteLine(rocket1._fuel); // Saída esperada: 500 L
+
+    rocket1.FuelAsPercentage = 101; // Saída esperada: Unhandled exception. System.ArgumentOutOfRangeException: Fuel percentage must be between 0 and 100% (Parameter 'Value')
+    Console.WriteLine(rocket1.FuelAsPercentage);
+    Console.WriteLine(rocket1._fuel);
+  }
+}
---
+++ 12-static-classes-and-methods/Rocket/Rocket.cs
@@ -0,0 +1,19 @@
+namespace RocketFactory;
+
+public class Rocket
+{
+  public int _fuel;
+  readonly int _maxFuel = 5000;
+
+  public int FuelAsPercentage
+  {
+    get { return _fuel * 100 / _maxFuel; }
+    set
+    {
+      if (value < 0 || value > 100)
+        throw new ArgumentOutOfRangeException("Value", "Fuel percentage must be between 0 and 100%");
+
+        _fuel = value * _maxFuel / 100;
+    }
+  }
+}
