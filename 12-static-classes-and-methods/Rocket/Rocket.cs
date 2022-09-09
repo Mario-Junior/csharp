648a3a6e4fb0fdff3b686eebc6e7f92c63dfddb5
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-09 11:13:58 -0300

    feat: static classes and methods exercises

--- 12-static-classes-and-methods/Rocket/Rocket.cs
+++ 12-static-classes-and-methods/Rocket/Rocket.cs
@@ -13,7 +13,7 @@ public class Rocket
       if (value < 0 || value > 100)
         throw new ArgumentOutOfRangeException("Value", "Fuel percentage must be between 0 and 100%");
 
-        _fuel = value * _maxFuel / 100;
+        _fuel = (value * _maxFuel) / 100;
     }
   }
 }
