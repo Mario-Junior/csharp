83c603d20b4197db922213bbe46d56afc5634a9a
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-10 22:41:03 -0300

    test: static classes and methods exercises

--- 12-static-classes-and-methods/Car.Test/CarTest.cs
+++ 12-static-classes-and-methods/Car.Test/CarTest.cs
@@ -1,10 +1,19 @@
 namespace Car.Test;
 
-public class UnitTest1
+public class CarTest
 {
     [Fact]
-    public void Test1()
+    public void ShouldBeAbleToConvertTopSpeed()
     {
+        Car car = new() { TopSpeed = 100 };
 
+        car.TopSpeed.Should().Be(100);
+        car.TopSpeedMph.Should().BeApproximately(62.13, 0.1);
+    }
+
+    [Fact]
+    public void HasKphToMphStaticMethod()
+    {
+        Car.KphToMph(100).Should().BeApproximately(62.13, 0.1);
     }
 }
