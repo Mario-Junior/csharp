d3abd3f4c81d80213fa4f409a27ce793e4aad6dc
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-12 08:58:38 -0300

    feat: abstract classes and polymorphism exercises

--- 14-abstract-classes-and-polymorphism/Robot/Program.cs
+++ 14-abstract-classes-and-polymorphism/Robot/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RobotFactory;
+
+class Program
+{
+  static void Main()
+  {
+    CloneControl control = new();
+    Robot robot = new()
+    {
+      ProcessingPower = 50,
+      BatteryCapacity = 100
+    };
+
+    control.CreateClone(robot);
+    control.CreateClone(robot);
+    control.CreateClone(robot);
+
+// Como conseguir acessar os itens dos objetos dentro do Array?
+    foreach (var item in control.Clones)
+    {
+      Console.WriteLine(item);
+    }
+  }
+}
