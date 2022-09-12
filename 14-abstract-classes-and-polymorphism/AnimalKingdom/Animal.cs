7d7751b467af6d2ac0e2d35fd2f8110de3f95be6
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-12 09:40:29 -0300

    feat: abstract classes and polymorphism exercises

---
+++ 14-abstract-classes-and-polymorphism/AnimalKingdom/Animal.cs
@@ -0,0 +1,16 @@
+namespace AnimalKingdom;
+
+public abstract class Animal
+{
+  public int Age { get; set; }
+  public abstract string EatingHabits { get; set; }
+  public abstract string Habitat { get; set; }
+
+  public abstract void Feed();
+  public abstract void Move();
+
+  public void Sleep()
+  {
+    Console.WriteLine("Zzzzz");
+  }
+}
