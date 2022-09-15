f3e6fa7f6dc1f1e69837857da9129cdc0f63b063
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-15 08:49:33 -0300

    feat: access modifiers exercises

---
+++ 15-access-modifiers/Private/Program.cs
@@ -0,0 +1,17 @@
+﻿public class A
+{
+  private string _item = "variável privada";
+}
+
+class B : A
+{
+  static void Main()
+  {
+    A a = new();
+    B b = new();
+
+    // DESCOMENTAR PARA VER O ERRO! Onde não é possível acessar as propriedades privadas de "a" nem de "b" herdadas de "a"
+    a._item = "acessando a variável item"; // Erro: "A._item" é inacessível devido ao seu nível de proteção [Private]csharp(CS0122)
+    b._item = "acessando a variável item"; // Erro: "B._item" é inacessível devido ao seu nível de proteção [Private]csharp(CS0122)
+  }
+}
