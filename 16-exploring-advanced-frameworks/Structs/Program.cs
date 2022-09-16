463d8c5c6dc1d5879c0280dfe767bca772566024
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-16 07:56:31 -0300

    feat: exploring advanced frameworks exercises

--- 16-exploring-advanced-frameworks/Structs/Program.cs
+++ 16-exploring-advanced-frameworks/Structs/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Coordinate someLocation = new(-19.922072, -43.939879);
+Console.WriteLine("Latitude: " + someLocation.Latitude);
+Console.WriteLine("Longitude: " + someLocation.Longitude);
+
+Coordinate anotherLocation = new();
+Console.WriteLine("Latitude: " + anotherLocation.Latitude);
+Console.WriteLine("Longitude: " + anotherLocation.Longitude);
+
+struct Point
+{
+  double x;
+  double y;
+}
+
+struct Coordinate
+{
+  public double Latitude;
+  public double Longitude;
+
+  public Coordinate(double latitude, double longitude)
+  {
+    Latitude = latitude;
+    Longitude = longitude;
+  }
+}
