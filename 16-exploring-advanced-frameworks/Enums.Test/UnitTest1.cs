57bfa803b7b2576eeb3861fb11bd34e5f3bbe8f5
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-16 08:49:55 -0300

    test: exploring advanced frameworks exercises

---
+++ 16-exploring-advanced-frameworks/Enums.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Enums.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ 16-exploring-advanced-frameworks/Enums/Games.cs
@@ -0,0 +1,23 @@
+namespace Enums;
+
+public enum GameType
+{
+  Action,
+  Adventure,
+  Puzzle,
+  Strategy,
+  Simulation,
+  Sports,
+  Other
+};
+
+public class Game
+{
+  public string? Name;
+  public GameType Type = GameType.Other;
+
+  public Game(GameType type)
+  {
+    Type = type;
+  }
+}
