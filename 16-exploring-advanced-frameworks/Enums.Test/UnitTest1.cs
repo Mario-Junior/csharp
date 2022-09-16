8b5b7c51b851b3acd13ae5ac54cba09d33bc717f
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-16 09:02:28 -0300

    test: exploring advanced frameworks exercises

--- 16-exploring-advanced-frameworks/Enums.Test/UnitTest1.cs
+++ 16-exploring-advanced-frameworks/Enums.Test/UnitTest1.cs
@@ -2,9 +2,32 @@ namespace Enums.Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory(DisplayName = "Deve conter um valor para o tipo de jogo")]
+    [InlineData(GameType.Action)]
+    [InlineData(GameType.Adventure)]
+    [InlineData(GameType.Sports)]
+    [InlineData(GameType.Other)]
+    public void ShouldContainGameType(GameType gameType)
     {
+        Game game = new(gameType);
 
+        game.Type.Should().Be(gameType);
+        game.Type.ToString().Should().BeEquivalentTo(gameType.ToString());
+    }
+
+    [Theory(DisplayName = "Deve conter um índice para o tipo de jogo")]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void ShouldContainGameTypeByIndex(int gameTypeIndex)
+    {
+        var gameTypeByIndex = (GameType)gameTypeIndex;
+
+        Game game = new(gameTypeByIndex);
+
+        game.Type.Should().Be(gameTypeByIndex);
+        game.Type.Should().Be((GameType)gameTypeIndex);
+        game.Type.ToString().Should().BeEquivalentTo(gameTypeByIndex.ToString());
     }
 }
