28cdd1089b5ccfbfb7be03110f0de39c7df6b3f2
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-16 08:03:16 -0300

    feat: exploring advanced frameworks exercises

--- 16-exploring-advanced-frameworks/Enums/Program.cs
+++ 16-exploring-advanced-frameworks/Enums/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Order myOrder = new();
+myOrder.PayType = PaymentType.Credit;
+
+enum PaymentType
+{
+  Credit = 0,
+  Debit = 1
+}
+
+// Lembrando que, caso eu não defina valores, eles iniciarão como uma lista, sendo o primeiro valor "0" (zero)
+// e os demais, incrementando de um em um
+
+class Order
+{
+  public PaymentType PayType;
+}
