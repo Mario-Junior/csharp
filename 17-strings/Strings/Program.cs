7ca58efdfb062b80ed42e62b7a85d2898d3b55fe
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-26 08:20:42 -0300

    feat: type manipulation exercises

---
+++ 17-strings/Strings/Program.cs
@@ -0,0 +1,6 @@
+﻿string textOne = "Você está aprendendo sobre ";
+string textTwo = "Strings em C#, ";
+string textThree = "e agora sabe concatenar textos utilizando a função Concat()!";
+
+string concatResult = string.Concat(textOne, textTwo, textThree);
+Console.WriteLine(concatResult);
