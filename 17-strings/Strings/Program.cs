e6be7409386719b5846f8ca25f45f86cde8c2840
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-26 08:27:07 -0300

    feat: type manipulation exercises

--- 17-strings/Strings/Program.cs
+++ 17-strings/Strings/Program.cs
@@ -1,6 +1,17 @@
-﻿string textOne = "Você está aprendendo sobre ";
+﻿// string.Concat()
+string textOne = "Você está aprendendo sobre ";
 string textTwo = "Strings em C#, ";
 string textThree = "e agora sabe concatenar textos utilizando a função Concat()!";
 
 string concatResult = string.Concat(textOne, textTwo, textThree);
 Console.WriteLine(concatResult);
+
+
+// string.Split()
+string emails = "email1@trybe.com;email2@trybe.com;email3@trybe.com";
+
+string[] arrayEmails = emails.Split(";");
+foreach (var email in arrayEmails)
+{
+  Console.WriteLine(email);
+}
