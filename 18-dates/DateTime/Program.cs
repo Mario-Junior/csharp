65c0484096368186fc58c75ca64efeba4975d910
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-28 20:52:01 -0300

    feat: type manipulation exercises

--- 18-dates/DateTime/Program.cs
+++ 18-dates/DateTime/Program.cs
@@ -4,5 +4,15 @@
     {
         var date = new DateTime(2022, 10, 2, 8, 35, 0);
         Console.WriteLine(date.ToString());
+
+        var dataType = DateTime.Now;
+        Console.WriteLine(dataType.GetType());
+
+        Console.WriteLine(getTimeNow());
+    }
+
+    public static string getTimeNow()
+    {
+        return "O momento de tempo atual é " +DateTime.Now;
     }
 }
