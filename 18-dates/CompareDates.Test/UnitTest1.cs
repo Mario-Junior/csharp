155a856c652e793faa581a16ff4999220987e5b8
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-29 07:10:02 -0300

    test: type manipulation [dates] exercises

--- 18-dates/CompareDates.Test/UnitTest1.cs
+++ 18-dates/CompareDates.Test/UnitTest1.cs
@@ -2,9 +2,19 @@ namespace CompareDates.Test;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    
+    [Theory(DisplayName = "Deve retornar o relacionamento entre as datas")]
+    [InlineData("2012/10/21", "2022/10/21", "antes")]
+    [InlineData("2011/10/21", "2001/10/21", "depois")]
+    [InlineData("2022/10/21", "2022/10/21", "igual")]
+    public void TestCompareDates(string date1, string date2, string expected)
     {
+        DatesCompare dates = new();
 
+        var d1 = DateTime.Parse(date1);
+        var d2 = DateTime.Parse(date2);
+        var result = dates.CompareDates(d1, d2);
+        result.Should().Be(expected);
     }
+
 }
