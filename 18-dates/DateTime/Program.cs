68426b1d418cc907a888362d7d1e1a9388423e69
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-28 21:08:47 -0300

    feat: type manipulation exercises

--- 18-dates/DateTime/Program.cs
+++ 18-dates/DateTime/Program.cs
@@ -25,13 +25,29 @@
         // De forma similar, existem as propriedades .Hour, .Minute, .Second, .Millisecond e .DayOfWeek
         // que retornam os componentes de Hora, Minuto, Segundo, Milissegundo e Dia da semana, respectivamente
 
-        // .Add(TimeSpan value)
+        // .Add(TimeSpan value) => O valor pode ser positivo ou negativo
         var today = DateTime.Now;
-        var duration = new TimeSpan(36, 0, 0, 0);
-        var answer = today.Add(duration);
+        var duration_pos = new TimeSpan(36, 0, 0, 0);
+        var duration_neg = new TimeSpan(-36, 0, 0, 0);
+        var answer_pos = today.Add(duration_pos);
+        var answer_neg = today.Add(duration_neg);
 
         Console.WriteLine("Hoje é " +today.Day +"/" +today.Month +" - " +today.DayOfWeek);
-        Console.WriteLine("Daqui a 36 dias será "+answer.Day +"/" +answer.Month +" - " +answer.DayOfWeek);
+        Console.WriteLine("Daqui a 36 dias será "+answer_pos.Day +"/" +answer_pos.Month +" - " +answer_pos.DayOfWeek);
+        Console.WriteLine("36 dias atrás era "+answer_neg.Day +"/" +answer_neg.Month +" - " +answer_neg.DayOfWeek);
+
+        // .Compare(DateTime t1, DateTime t2)
+        DateTime date1 = new DateTime(2010, 9, 1, 5, 0, 0);
+        DateTime date2 = new DateTime(2022, 8, 10, 12, 0, 0);
+
+        int result = DateTime.Compare(date1, date2);
+        string relationship;
+
+        if (result < 0) relationship = "é anterior à";
+        else if (result == 0) relationship = "è o mesmo que";
+        else relationship = "è posterior à";
+
+        Console.WriteLine("{0} {1} {2}", date1, relationship, date2);
     }
 
     public static string getTimeNow()
