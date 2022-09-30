e3d21c8064bef05b2190375f36f29d97029d2d29
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-30 07:36:00 -0300

    feat: type manipulation [advanced types] exercises

--- 19-advanced-types/GenericList/Program.cs
+++ 19-advanced-types/GenericList/Program.cs
@@ -11,5 +11,14 @@ public class Program
     listString.Add("Hallo");
 
     listString.Print();
+
+    // AnonymousTypes:
+    Console.WriteLine("AnonymousTypes:");
+    Console.WriteLine("Exemplo: var myAnonymousType = new { Amount = 42, Message = 'Olá',  Value = 3.95};");
+    var myAnonymousType = new { Amount = 42, Message = "Olá",  Value = 3.95};
+
+    Console.WriteLine("Console: " + myAnonymousType.Amount);
+    Console.WriteLine("Console: " + myAnonymousType.Message);
+    Console.WriteLine("Console: " + myAnonymousType.Value);
   }
 }
