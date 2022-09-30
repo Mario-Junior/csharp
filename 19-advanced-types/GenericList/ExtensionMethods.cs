c1ae5fb940a009e37062d1ed96c129144e71c5c0
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-09-30 08:04:48 -0300

    feat: type manipulation [advanced types] exercises

---
+++ 19-advanced-types/GenericList/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+namespace genericList;
+
+public static class ExtensionMethods
+{
+  public static int WordCount1(string str)
+  {
+    Console.WriteLine("--------------------------------------------------------------------------------");
+    Console.WriteLine("Extension Methods (ver o arquivo da classe pra entender (sem e com extensão)):");
+    var splitted = str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries);
+    return splitted.Length;
+  }
+
+  public static int WordCount2(this string str)
+  {
+    var splitted = str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries);
+    return splitted.Length;
+  }
+}
--- 19-advanced-types/GenericList/Program.cs
+++ 19-advanced-types/GenericList/Program.cs
@@ -13,5 +13,12 @@ public class Program
     listString.Print();
 
     AnonymousTypes.AnonymousTypesExample();
+
+    var message = "Olá, Mundo!";
+    var words1 = ExtensionMethods.WordCount1(message);
+    Console.WriteLine("Console com método SEM extensão: " + words1);
+    
+    var words2 = message.WordCount2();
+    Console.WriteLine("Console com método COM extensão: " + words2);
   }
 }
