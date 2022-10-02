e739631fd549936502820b0de330f7ca3a5f20fc
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-02 20:52:39 -0300

    feat: csharp data collections exercises

---
+++ 20-csharp-data-collections/List/Program.cs
@@ -0,0 +1,12 @@
+﻿// Coleções de dados em C#
+// São elas:
+// Arraylist: São matrizes de objetos que aumentam o seu tamanho dinamicamente.
+// Hashtable: Coleções de chave-valor organizadas com base no código hash da chave.
+
+List<string> stringList = new List<string>(){ "carro", "casa", "roupa", "avião" };
+
+List<int> intList = new List<int>{ 1, 2, 6, 8, 10, 87, 45 };
+
+List<string> animals = new List<string>(){ "cachorro", "gato",  "pássaro", "peixe" };
+
+Console.WriteLine(animals[0]);
