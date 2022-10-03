bdff13469272c249af3a4ff2e2a43994e5079635
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-03 10:31:41 -0300

    feat: LINQ basics exercises

--- 21-linq-basics/LinqBasics/Program.cs
+++ 21-linq-basics/LinqBasics/Program.cs
@@ -17,9 +17,12 @@ public class Program
   {
     Console.WriteLine("% Declarative Linq query:");
     DeclarativeLinq();
-    Console.WriteLine("-------------------------");
+    Console.WriteLine("--------------------------------");
     Console.WriteLine("% Lambda Linq query:");
     LambdaLinq();
+    Console.WriteLine("--------------------------------");
+    Console.WriteLine("% Lambda Linq refactored query:");
+    LambdaLinqRefactored();
   }
   static void DeclarativeLinq()
   {
@@ -40,6 +43,18 @@ public class Program
     string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
 
     IEnumerable<string> filteredGames = System.Linq.Enumerable.Where(games, n => n.Contains('a'));
+
+    foreach (string game in filteredGames)
+    {
+      Console.WriteLine(game);
+    }
+  }
+
+  static void LambdaLinqRefactored()
+  {
+    string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
+
+    var filteredGames = games.Where(n => n.Contains('a'));
 
     foreach (string game in filteredGames)
     {
