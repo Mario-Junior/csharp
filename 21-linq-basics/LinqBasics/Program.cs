fcb1ecc317dca7a5a02e519d31a99628828c73e3
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-03 10:14:17 -0300

    feat: LINQ basics exercises

--- 21-linq-basics/LinqBasics/Program.cs
+++ 21-linq-basics/LinqBasics/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
+
+// Consulta completa:
+IEnumerable<string> filteredGames1 = from game in games
+                                        where game.Contains('a')
+                                        select game;
+
+// Consulta simplificada (usando tipos implícitos):
+var filteredGames2 = from game in games
+                  where game.Contains('a')
+                  select game;
