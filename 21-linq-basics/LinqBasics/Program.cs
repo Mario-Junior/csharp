67e5eaa39c86b9153416d7f503ab1a12472469bd
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-03 10:27:35 -0300

    feat: LINQ basics exercises

--- 21-linq-basics/LinqBasics/Program.cs
+++ 21-linq-basics/LinqBasics/Program.cs
@@ -1,11 +1,49 @@
-﻿string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
+﻿namespace LinqBasics;
+using System;
+using System.Linq;
+// DESCOMENTE para ver os modelos de consulta Declarativas:
+// // Consulta completa:
+// IEnumerable<string> filteredGames1 = from game in games
+//                                         where game.Contains('a')
+//                                         select game;
 
-// Consulta completa:
-IEnumerable<string> filteredGames1 = from game in games
-                                        where game.Contains('a')
-                                        select game;
+// // Consulta simplificada (usando tipos implícitos):
+// var filteredGames2 = from game in games
+//                   where game.Contains('a')
+//                   select game;
+public class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("% Declarative Linq query:");
+    DeclarativeLinq();
+    Console.WriteLine("-------------------------");
+    Console.WriteLine("% Lambda Linq query:");
+    LambdaLinq();
+  }
+  static void DeclarativeLinq()
+  {
+    string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
 
-// Consulta simplificada (usando tipos implícitos):
-var filteredGames2 = from game in games
+    var filteredGames = from game in games
                   where game.Contains('a')
                   select game;
+
+    foreach (string game in filteredGames)
+    {
+      Console.WriteLine(game);
+    }
+  }
+
+  static void LambdaLinq()
+  {
+    string[] games = {"Fortnite", "Valorant", "Destiny", "Call of Duty", "World of Warcraft"};
+
+    IEnumerable<string> filteredGames = System.Linq.Enumerable.Where(games, n => n.Contains('a'));
+
+    foreach (string game in filteredGames)
+    {
+      Console.WriteLine(game);
+    }
+  }
+}
