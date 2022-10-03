7a023ce0ccf5d90f61294f4d420ecb7936d7a42f
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-02 21:58:25 -0300

    test: csharp data collections exercises

--- 20-csharp-data-collections/Garage.Test/UnitTest1.cs
+++ 20-csharp-data-collections/Garage.Test/UnitTest1.cs
@@ -34,4 +34,49 @@ public class UnitTest1
         // Verifica se a quantidade retornada é igual a quantidade de veídulos esperados
         numberVehicles.Should().Be(vehiclesExpected.Count);
     }
+
+    public static TheoryData<List<string>, string, List<string>> TestRemoveVehicleData = new()
+    {
+        {
+            new List<string>{"Bicicleta", "Moto", "Carro"},
+
+            // Removendo a Moto
+            "Moto",
+
+            // Lista sem a Moto
+            new List<string>{"Bicicleta", "Carro"}
+        }
+    };
+
+    [Theory(DisplayName = "Deve remover corretamente um carro da garagem")]
+    [MemberData(nameof(TestRemoveVehicleData))]
+    public void TestRemoveVehicle(List<string> vehiclesEntry, string vehicle, List<string> vehiclesExpected)
+    {
+        var instance = new Garage();
+        instance.Vehicles = vehiclesEntry;
+        var numberVehicles = instance.RemoveVehicle(vehicle);
+
+        instance.Vehicles.Should().BeEquivalentTo(vehiclesExpected);
+        numberVehicles.Should().Be(vehiclesExpected.Count);
+    }
+
+    public static TheoryData<List<string>, string, int> TestFindVehiclePositionData = new()
+    {
+        {
+            new List<string>{"Bicicleta", "Moto", "Carro"},
+            "Carro",
+            2
+        }
+    };
+
+    [Theory(DisplayName = "Deve encontrar a posição do carro na garagem")]
+    [MemberData(nameof(TestFindVehiclePositionData))]
+    public void TestFindVehiclePosition(List<string> vehiclesEntry, string vehicle, int position)
+    {
+        var instance = new Garage();
+        instance.Vehicles = vehiclesEntry;
+        var numberVehicles = instance.FindVehiclePosition(vehicle);
+        
+        numberVehicles.Should().Be(position);
+    }
 }
