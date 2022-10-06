0aaa2b99b600c77c9e59827454f1659a6ee80cdd
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-06 10:08:07 -0300

    test: linq advanced queries exercises

--- 22-linq-advanced/Brazil.Test/BrazilTest.cs
+++ 22-linq-advanced/Brazil.Test/BrazilTest.cs
@@ -2,28 +2,32 @@ namespace Brazil.Test;
 
 public class BrazilTest
 {
-    [Theory(DisplayName = "GetNumberOfCitiesForEachState deveria retornar o número de cidades para cada estado")]
-    [InlineData(
-        new City[] {
+    readonly public static TheoryData<City[], State[], dynamic[]> TestData = new()
+    {
+        {
+          new City[] {
             new City { Name="Belém", StateAbbreviation="PA" },
             new City { Name="Santarém", StateAbbreviation="PA" },
             new City { Name="Rio Branco", StateAbbreviation="AC" },
             new City { Name="Maceió", StateAbbreviation="AL" },
-        },
-        new State[] {
-            new State { Name="Pará", Abbreviation="PA", Region="Norte" },
-            new State { Name="Acre", Abbreviation="AC", Region="Norte" },
-            new State { Name="Alagoas", Abbreviation="AL", Region="Nordeste" },
-        },
-        new[] {
-            new { StateAbbreviation = "PA", NumberOfCities = 2 },
-            new { StateAbbreviation = "AC", NumberOfCities = 1 },
-            new { StateAbbreviation = "AL", NumberOfCities = 1 },
+            },
+            new State[] {
+                new State { Name="Pará", Abbreviation="PA", Region="Norte" },
+                new State { Name="Acre", Abbreviation="AC", Region="Norte" },
+                new State { Name="Alagoas", Abbreviation="AL", Region="Nordeste" },
+            },
+            new[] {
+                new { StateAbbreviation = "PA", NumberOfCities = 2 },
+                new { StateAbbreviation = "AC", NumberOfCities = 1 },
+                new { StateAbbreviation = "AL", NumberOfCities = 1 },
+            }
         }
-    )]
+    };
+    [Theory(DisplayName = "GetNumberOfCitiesForEachState deveria retornar o número de cidades para cada estado")]
+    [MemberData(nameof(TestData))]
     public void TestGetNumberOfCitiesForEachState(City[] citiesEntry, State[] statesEntry, dynamic[] expected)
     {
-        var instance = new Brazil();
+        Brazil instance = new();
         instance.cities = citiesEntry;
         instance.states = statesEntry;
         var result = instance.GetNumberOfCitiesForEachState();
