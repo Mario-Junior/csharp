83bbfb985a2779557c1724f06fb76c7dd202bde2
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-05 21:03:53 -0300

    feat: linq advanced queries exercises

--- 22-linq-advanced/City.cs
+++
@@ -1,5 +0,0 @@
-public class City
-{
-  public string Name { get; set; } = "";
-  public string StateAbbreviation { get; set; } = "";
-}
--- 22-linq-advanced/State.cs
+++
@@ -1,6 +0,0 @@
-public class State
-{
-  public string Name { get; set; } = "";
-  public string Abbreviation { get; set; } = "";
-  public string Region { get; set; } = "";
-}
