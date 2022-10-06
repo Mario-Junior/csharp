df6be4f46577a07ff390f3550d77f25100751ce2
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-06 06:51:07 -0300

    feat: linq advanced queries exercises

---
+++ 22-linq-advanced/Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿// Os métodos de uma coleção
+
+// Base de dados é um array de inteiros
+int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+// numQuery é do tipo IEnumerable<int>
+// Selecionando apenas os números pares da coleção.
+var numQuery =
+    from num in numbers
+    where (num % 2) == 0
+    select num;
