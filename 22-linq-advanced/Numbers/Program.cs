245d8f9ddc21188130fc2a8c1f384be2922b28e3
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-06 06:57:23 -0300

    feat: linq advanced queries exercises

--- 22-linq-advanced/Numbers/Program.cs
+++ 22-linq-advanced/Numbers/Program.cs
@@ -9,3 +9,11 @@ var numQuery =
     from num in numbers
     where (num % 2) == 0
     select num;
+
+// Count():
+// Método para qualquer tipo de consulta e coleções.
+// Retorna o número de elementos da consulta ou lista que implementa a Interface IEnumerable
+Console.WriteLine("$ Método .Count:");
+Console.WriteLine("int[] numbers.Count() = " + numbers.Count());
+Console.WriteLine("numQuery.Count() = " + numQuery.Count());
+Console.WriteLine("------------------------------------------------------------------------");
