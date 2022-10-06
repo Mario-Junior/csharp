12c00188ec1a7e7d7ce10c86f4efdc9910d661df
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-06 08:33:42 -0300

    test: linq advanced queries exercises

---
+++ 22-linq-advanced/Brazil.Test/Brazil.cs
@@ -0,0 +1,21 @@
+namespace Brazil;
+
+public class Brazil
+{
+  public State[]? states;
+  public City[]? cities;
+
+  public dynamic[] GetNumberOfCitiesForEachState()
+  {
+    var numberOfCitiesForEachState =
+      from state in states
+      join city in cities on state.Abbreviation equals city.StateAbbreviation
+      group city by state.Abbreviation into stateGroup
+      select new {
+          StateAbbreviation = stateGroup.Key,
+          NumberOfCities = stateGroup.Count()
+      };
+
+    return numberOfCitiesForEachState.ToArray();
+  }
+}
