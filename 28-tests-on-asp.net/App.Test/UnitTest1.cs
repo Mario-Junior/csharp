62b8bbb0b364bdd2eb9519803b611f7b69a2d5d6
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-17 08:09:02 -0300

    test: tests on asp.net exercises

--- 28-tests-on-asp.net/App.Test/UnitTest1.cs
+++ 28-tests-on-asp.net/App.Test/UnitTest1.cs
@@ -10,6 +10,17 @@ public class TestWeatherForecast : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Theory(DisplayName = "WeatherForecast deve responder com status 200 ~ 299 para rotas")]    
+    [InlineData("/")]
+    public async Task GetInitialEndpointReturnSuccess(string url)
+    {        
+        var client = _factory.CreateClient();
+        var response = await client.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+        response.Content.Headers.ContentType!.ToString().Should().Be("text/plain; charset=utf-8");
+    }
+
+    [Theory(DisplayName = "WeatherForecast deve responder com status 200 ~ 299 para rotas")]    
     [InlineData("/weatherforecast")]
     public async Task GetEndpointsReturnSuccess(string url)
     {        
