37a6f1797bbfcd7e82804c40b95686d973f8cae5
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-21 20:13:03 -0300

    feat: entity framework repositories exercises

--- 31-entity-framework-repositories/Library/Models/DatabaseContext.cs
+++ 31-entity-framework-repositories/Library/Models/DatabaseContext.cs
@@ -4,8 +4,27 @@ namespace Library;
 
 public class DatabaseContext : DbContext
 {
+  public DbSet<Book> Books { get; set; }
+
+  // Para acessar somente a classe mãe Book e, a partir dela, fazer as consultas nas classes agregadas!
+  // public DbSet<Publisher> Publisher { get; set; }
+
+  // public DbSet<Author> Author { get; set; }
   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
   {
     optionsBuilder.UseSqlServer(@"Server=127.0.0.1;Database=master;User=SA;Password=123@SQLServer");
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<Book>()
+      .HasOne(b => b.Author) // Explicita que a entidade Book tem uma referência a Author
+      .WithMany(a => a.Books) // Define a relação como um Author para muitos Book
+      .HasForeignKey(b => b.AuthorId); // Define como chave estrangeira a variável AuthorId
+
+    modelBuilder.Entity<Book>()
+      .HasOne(b => b.Publisher)// Explicita que a entidade Book tem uma referência a Publisher
+      .WithMany(p => p.Books) // Define a relação como um Publisher para muitos Book
+      .HasForeignKey(b => b.PublisherId); // Define como chave estrangeira a variável PublisherId
   }
 }
