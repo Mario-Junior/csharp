542abafb476e2c4022a19f0c217b058eea3eaaba
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-21 19:49:59 -0300

    feat: entity framework repositories exercises

--- 31-entity-framework-repositories/Library/Models/DatabaseContext.cs
+++ 31-entity-framework-repositories/Library/Models/DatabaseContext.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 
+namespace Library;
+
 public class DatabaseContext : DbContext
 {
-  
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseSqlServer(@"Server=127.0.0.1;Database=master;User=SA;Password=123@SQLServer");
+  }
 }
