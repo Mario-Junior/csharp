d262b370f72f01e8271ab7e2679505dfe86ef2d3
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-10-22 11:36:08 -0300

    feat: entity framework repositories exercises

--- 31-entity-framework-repositories/Library/Program.cs
+++ 31-entity-framework-repositories/Library/Program.cs
@@ -9,12 +9,16 @@ class Program
     
     context.Database.EnsureCreated();
 
+    Console.WriteLine(">>> Trazendo todos os itens do BD");
     var books = bookRepository.GetBookList();
     foreach (var book in books)
     {
       Console.WriteLine(
         $"Id: {book.BookId} - Title: {book.Title} - Pages: {book.Pages} - Year: {book.Year}");
     }
+
+    Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+    Console.WriteLine(">>> Adicionando novo item e recuperando pelo Id");
 
     var newBook = new Book{
       Title = "The Divine Comedy",
@@ -37,12 +41,27 @@ class Program
     Console.WriteLine(
         $"Id: {bookById.BookId} - Title: {bookById.Title} - Author: {bookById.Author?.Name} - Publisher: {bookById.Publisher?.Name}");
 
+    Console.WriteLine(">>> Trazendo todos os itens do BD");
+    var books2 = bookRepository.GetBookList();
+    foreach (var book in books2)
+    {
+      Console.WriteLine(
+        $"Id: {book.BookId} - Title: {book.Title} - Pages: {book.Pages} - Year: {book.Year}");
+    }
+
+    Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+    Console.WriteLine(">>> Atualizando detalhes de um item e comprovando a atualização");
+
     var bookToUpdate = bookRepository.GetById(3);
     bookToUpdate.Publisher!.Name = "Publisky";
     bookRepository.Update(bookToUpdate);
 
     Console.WriteLine(
         $"Id: {bookById.BookId} - Title: {bookById.Title} - Author: {bookById.Author?.Name} - Publisher: {bookById.Publisher?.Name}");
+
+
+    Console.WriteLine("------------------------------------------------------------------------------------------------------------");
+    Console.WriteLine(">>> Deletando um item e comprovando a exclusão");
 
     bookRepository.Delete(3);
 
