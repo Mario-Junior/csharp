a0254bb44a8c58980885584c0eb702307fc0f2a4
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-11-04 10:42:03 -0300

    test: authentication exercises

--- 33-authentication/Authentication.Test/AuthenticationTest.cs
+++ 33-authentication/Authentication.Test/AuthenticationTest.cs
@@ -1,6 +1,7 @@
 using Auth.Models;
 using Auth.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
 
 namespace Authentication.Test;
 
@@ -19,5 +20,34 @@ public class UnitTest1
 
         var response = new AuthController().Authenticate(user);
         response.Value.Token.Should().NotBeNull();
+    }
+
+    [Theory]
+    [InlineData("Luiz", "trybes2")]
+    [InlineData("Mayara", "trybe1")]
+    [InlineData("Rahel", "123456")]
+    [InlineData("Marina", "software")]
+    public void TestAuthenticateNotFoundFail(string name, string password)
+    {
+        User user = new() 
+        {
+        Name = name,
+        Password = password
+        };
+
+        var response = new AuthController().Authenticate(user);
+        response.Result.Should().BeOfType<NotFoundObjectResult>();
+    }
+
+    [Theory]
+    [InlineData("Maria", "")]
+    [InlineData("", "trybe1")]
+    [InlineData("", "")]
+    public void TestAuthenticateBadRequestFail(string name, string password)
+    {
+        User user = null;
+
+        var response = new AuthController().Authenticate(user);
+        response.Result.Should().BeOfType<BadRequestObjectResult>();
     }
 }
