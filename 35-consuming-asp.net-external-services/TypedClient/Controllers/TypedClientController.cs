26efc7d8b976c7e39efe0aa9936a55e78a87f4a8
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-11-15 20:19:00 -0300

    feat: consuming external services exercises

---
+++ 35-consuming-asp.net-external-services/TypedClient/Controllers/TypedClientController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using TypedClient.Services;
+
+namespace TypedClient.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TypedClientController : ControllerBase
+{
+  private ICepService? _cepService;
+  public TypedClientController(ICepService cepService)
+  {
+    _cepService = cepService;
+  }
+
+  [HttpGet]
+  [Route("{cep}")]
+  public async Task<IActionResult> GetCep(string cep)
+  {
+    var address = await _cepService.GetCep(cep);
+    if (address == null)
+    {
+      return NotFound();
+    }
+
+    return Ok(address);
+  }
+}
