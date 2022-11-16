b7ffb9f1047831891662f02aa16599ad6051a599
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-11-15 21:06:44 -0300

    test: consuming external services exercises

---
+++ 35-consuming-asp.net-external-services/TypedClient.Test/TypedClientTestServer.cs
@@ -0,0 +1,45 @@
+using TypedClient.Services;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+using System.Text.Json;
+using System.Net.Http.Json;
+using TypedClient;
+
+namespace TypedClient.Test;
+
+public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+{
+  protected override void ConfigureWebHost(IWebHostBuilder builder)
+  {
+    builder.UseEnvironment("Testing");
+
+    base.ConfigureWebHost(builder);
+  }
+
+  protected override IHost CreateHost(IHostBuilder builder)
+  {
+    return base.CreateHost(builder);
+  }
+}
+
+public class TypedClientTestServer : IClassFixture<TestingWebAppFactory<Program>>
+{
+
+  private readonly HttpClient? _client;
+
+  public TypedClientTestServer(TestingWebAppFactory<Program> factory)
+        => _client = factory.CreateClient();
+
+    [Fact]
+    public async void ShouldMakeARequestWithTestServer()
+    {
+        var response = await _client.GetAsync("https://viacep.com.br/ws/01001000/json/");
+        // var result = await response.Content.ReadFromJsonAsync<object>();
+        
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        // result.Should().BeOfType<JsonElement>();
+    }
+}
